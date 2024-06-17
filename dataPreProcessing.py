0a448a03e76a1b6746c5470102b7becd4c1b4040
RohitRS3107 <72965804+RohitRS3107@users.noreply.github.com>
2024-06-17 19:58:39 +0530

    Update dataPreProcessing.py

--- dataPreProcessing.py
+++ dataPreProcessing.py
@@ -1,4 +1,4 @@
 from prepareData import prepare_dataset
-train_data = prepare_dataset('/home/thor/ASDUsingImage/train')
-validation_data = prepare_dataset('/home/thor/ASDUsingImage/valid')
-test_data = prepare_dataset('/home/thor/ASDUsingImage/test')
+train_data = prepare_dataset('/train')
+validation_data = prepare_dataset('/valid')
+test_data = prepare_dataset('/test')
