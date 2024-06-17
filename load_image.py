9fcfa1fb6062a416763c5bc60b5eb636c96399d2
RohitRS3107 <72965804+RohitRS3107@users.noreply.github.com>
2024-06-17 19:53:43 +0530

    Update load_image.py

--- load_image.py
+++ load_image.py
@@ -1,9 +1,9 @@
-def image(bhya):
+def image(i):
 
 	import numpy as np
 	from PIL import Image
 	from skimage import transform
-	img = Image.open(bhya)
+	img = Image.open(i)
 	img = np.array(img).astype('float32')/255
 	img = transform.resize(img, (244,244,3))
 	img = np.expand_dims(img, axis = 0)
