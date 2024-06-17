5f8facacc2ceb27193a52b354818cdac3c8b30cc
RohitRS3107 <72965804+RohitRS3107@users.noreply.github.com>
2024-06-17 19:56:02 +0530

    Update predict.py

--- predict.py
+++ predict.py
@@ -1,8 +1,8 @@
 from tensorflow import keras as tf
 
-model = tf.models.load_model('/home/thor/Downloads/vgg_model(1).h5')
+model = tf.models.load_model('/vgg_model.h5')
 from load_image import image
-img = image('/home/thor/ASDUsingImage/test/non_autistic/003.jpg')
+img = image('/test/non_autistic/003.jpg')
 
 res = model.predict(img).argmax()
 
